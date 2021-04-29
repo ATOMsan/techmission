2479543f7b8f5d4410cee08da28270eaa0e5c90d
Anton Demidov <gkukishek@gmail.com>
2021-04-29 08:45:17 +0300

    version new

--- app/js/dascripts.js
+++ app/js/dascripts.js
@@ -4,11 +4,69 @@ $(".da__open-tab-btn").on("click", function (e) {
     e.preventDefault();
     $(this).toggleClass("active");
     $(this).parent().parents(".da__program-tabs")
-    .find(".da__program-tab-info")
+    .find(".da__program-info-container")
     .toggleClass("active")
     .slideToggle(400);
     $(this).parent().parents(".da__program-tab").toggleClass("active");
- /*    $(this).siblings("faq__block").removeClass("active");
-    $(this).siblings("faq__block").find(".faq-text").slideUp(400); */
   });
+
+
+  $(".da__program-inner-tab .control").on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(this).parent().parents(".da__program-info-tab")
+    .find(".da__program-inner-tab-info")
+    .slideToggle(400);
+    $(this).parent().parents(".da__program-info-tab").toggleClass("active");
+  });
+
+
+  
+
+  const sliderDa = $(".comment__slider-da").slick({
+    slidesToShow: 2,
+    arrows: false,
+    slidesToScroll: 1,
+  /*   centerMode: false, */
+    variableWidth: true,
+    speed: 1000,
+    infinity: false,
+    swipe: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          centerMode: true,
+        },
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          centerMode: false,
+        },
+      },
+    ],
+  });
+
+  $(".prev-comment").on("click", (e) => {
+    e.preventDefault();
+    console.log("click");
+    sliderDa.slick("slickPrev");
+  });
+
+  $(".next-comment").on("click", (e) => {
+    e.preventDefault();
+    console.log("click");
+    sliderDa.slick("slickNext");
+  });
+
+
+  $(".read-more").on("click", function(e){
+    e.preventDefault();
+    $(this).parent().find("p").toggleClass("active");
+    $(this).toggleClass("active");
+  })
+  
 });
